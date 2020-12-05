e166d97a9637a6b1f775b082d1ed37e42df4a7ba
Adiyatma Koto <adiyatma.padang@gmail.com>
2020-12-05 17:15:26 +0700

    well commit regist beres

--- api_well.py
+++ api_well.py
@@ -12,44 +12,60 @@ from waitress import serve
 app = Flask(__name__)
 
 
-@app.route('/user/ambil', methods=['post'])
+@app.route('/user/regist', methods=['post'])
 def regist():
     json_data = flask.request.json
     if json_data == None:
         result = {"pesan": "Not available"}
         return jsonify(result)
     else:
-        id = json_data['id']
         nama = json_data['nama']
         nik = json_data['nik']
-        beras = json_data['beras']
-        sayur = json_data['sayur']
-        lauk = json_data['lauk']
+        card_uid = json_data['card_uid']
+        id_saldo = card_uid
         db = mysql.connector.connect(host="localhost",
                                      user="root",
                                      password="",
                                      database="rfidattendance")
-        check = check_id(id)
-        if check == 0:
-            result = {"pesan": "User belum terdaftar"}
+        check_Nik = check_nik(nik)
+        check_Id_saldo = check_id_saldo(card_uid)
+        if check_Nik == 1 and check_Id_saldo == 1:
+            result = {"pesan": "User sudah terdaftar"}
             return jsonify(result)
         else:
             cursor = db.cursor()
             cursor.execute(
-                "INSERT INTO `users`(`id`, `nama`, `nik`, `beras`, `sayur`, `lauk`) VALUES (%s,%s,%s,%s,%s,%s)",
-                (id, nama, nik, beras, sayur, lauk))
+                "INSERT INTO `users`(`nama`, `nik`, `card_uid`, `id_saldo`) VALUES(%s,%s,%s,%s)",
+                (nama, nik, card_uid, id_saldo))
+            cursor.execute(
+                "INSERT INTO `saldo`(`id_saldo`, `beras`, `sayur`, `lauk`) VALUES(%s,%s,%s,%s)",
+                (card_uid, 2, 1, 1))
             db.commit()
-            result = {"message": "input berhasil"}
+            result = {"pesan": "Daftar berhasil"}
             return jsonify(result)
 
 
-def check_id(a):
+def check_nik(a):
     db = mysql.connector.connect(host="localhost",
                                  user="root",
                                  password="",
                                  database="rfidattendance")
     cursor = db.cursor()
-    cursor.execute("SELECT id FROM admin where id=%s", (a, ))
+    cursor.execute("SELECT nik FROM users where nik=%s", (a, ))
+    a = cursor.fetchone()
+    if a == None:
+        return 0
+    else:
+        return 1
+
+
+def check_id_saldo(b):
+    db = mysql.connector.connect(host="localhost",
+                                 user="root",
+                                 password="",
+                                 database="rfidattendance")
+    cursor = db.cursor()
+    cursor.execute("SELECT id_saldo FROM saldo where id_saldo=%s", (b, ))
     a = cursor.fetchone()
     if a == None:
         return 0
@@ -58,5 +74,5 @@ def check_id(a):
 
 
 if __name__ == "__main__":
-    # serve(app, host="0.0.0.0", port=7000)
+    #serve(app, host="0.0.0.0", port=7000)
     app.run(port=7000, debug=True)
